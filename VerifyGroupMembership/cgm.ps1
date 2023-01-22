9a8c393bcb2030f90bac102a2104b5c26712a4e8
José Angel Arríaga <44209914+josearriaga@users.noreply.github.com>
2023-01-22 01:20:47 -0600

    Update cgm.ps1

--- VerifyGroupMembership/cgm.ps1
+++ VerifyGroupMembership/cgm.ps1
@@ -1,62 +1,64 @@
-# Connect to Exchange Online
-Connect-ExchangeOnline
+Connect-AzureAD
+
+# Retrieve a list of all the recipients in your organization
+Get-AzureADUser -All $true | select "UserPrincipalName" | ConvertTo-csv -NoTypeInformation | Out-File $PSScriptRoot\AllUsers.csv 
+$AllUsers = " $PSScriptRoot\AllUsers.csv "
+
+Start-Sleep -Seconds 20
+Write-Host "AllUsers: $AllUsers" -ForegroundColor Cyan
 
 # Prompt user for keyword
 $Keyword = Read-Host "Enter keyword to search for in group names"
+Write-Host "Keyword: $Keyword" -ForegroundColor Cyan
 
-# Set batch size
-$BatchSize = 100
+# Get all groups that match the keyword
+$Groups = Get-AzureADGroup -SearchString "$Keyword"
+Write-Host "Groups: $Groups" -ForegroundColor Cyan
 
-Write-Host "Retrieving distribution groups that contain the keyword in their name" -ForegroundColor Green
+# Loop through each group
+foreach ($Group in $Groups) {
+    Write-Host "Checking members of group: $($Group.displayName)" -ForegroundColor Magenta
 
-# Get distribution groups that contain the keyword in their name
-$DistributionGroups = Get-DistributionGroup -Filter {Name -like "*$Keyword*"} -ResultSize $BatchSize
-
-Write-Host "Retrieving mail-enabled security groups that contain the keyword in their name" -ForegroundColor Green
-
-# Get mail-enabled security groups that contain the keyword in their name
-$SecurityGroups = Get-Mailbox -Filter {Name -like "*$Keyword*"} -RecipientTypeDetails SecurityGroup -ResultSize $BatchSize
-
-Write-Host "Retrieving Microsoft 365 groups that contain the keyword in their name" -ForegroundColor Green
-
-# Get Microsoft 365 groups that contain the keyword in their name
-$UnifiedGroups = Get-UnifiedGroup -Filter {Name -like "*$Keyword*"} -ResultSize $BatchSize
-
-Write-Host "Creating an empty array to store the list of users" -ForegroundColor Green
-
-# Create an empty array to store the list of users
-$Users = @()
-
-Write-Host "Looping through each group and checking the members" -ForegroundColor Green
-
-# Loop through each group and check the members
-foreach ($Group in $DistributionGroups + $SecurityGroups + $UnifiedGroups) {
-    # Get the members of the group in batches
-    for ($i = 0; $i -lt $Group.MemberCount; $i += $BatchSize) {
-        $Members = Get-UnifiedGroupLinks -Identity $Group.Identity -LinkType Members -Paging $i -ResultSize $BatchSize
-        # Loop through each member and check if they are not in the group
-        foreach ($Member in $Members) {
-            if (!$Group.Members.Contains($Member.PrimarySmtpAddress)) {
-                # Add the member to the list of users
-                $Users += $Member
+    $Members = Get-AzureADGroupMember -All $true -ObjectId $Group.ObjectId | Where-Object {$_.UserType -eq 'member'}
+    Write-Host "Members: $Members" -ForegroundColor Cyan
+    if ($Members) {
+        # Creating an empty array to store the list of users not in groups
+        $UsersNotInGroups = @()
+        # Loop through each user in the $AllUsers variable
+        foreach ($User in $AllUsers) {
+            if ($Members.UserPrincipalName -notcontains $User.UserPrincipalName) {
+                $UsersNotInGroups += $User
             }
         }
     }
+        If ($UsersNotInGroups.Count -eq 0) {
+        Write-Host "No users were found that are not in group '$($Group.displayName)'"
+    } else {
+        Write-Host "Users not in group '$($Group.displayName)':" -ForegroundColor Cyan
+        $UsersNotInGroups | Format-Table -Property UserPrincipalName, DisplayName
+    }
 }
+
 
 $DefaultPath = "$PSScriptRoot\UsersNotInGroups.csv"
 
+# Prompt user for save location
 $SaveLocation = Read-Host "Do you want to save the csv in the directory the script is stored in? (Y/N)"
 
 if ($SaveLocation -eq "Y") {
+    $DefaultPath = (Join-Path $PSScriptRoot "UsersNotInGroups.csv")
     $SaveLocation = $DefaultPath
 } else {
-    $SaveLocation = Read-Host "Please specify the location where you want to save the CSV file"
+    $SaveLocation = Read-Host "Please specify the full path where you want to save the CSV file"
 }
 
-Write-Host "Exporting the list of users to a CSV file in batches" -ForegroundColor Green
+Write-Host "Exporting the list of users not in groups to a CSV file" -ForegroundColor Green
 
-# Export the list of users to a CSV file in batches
-for ($i = 0; $i -lt $Users.Count; $i += $BatchSize) {
-    $Users[$i..($i+$BatchSize-1)] | Export-Csv -Path $SaveLocation -NoTypeInformation -Append
+# Export the list of users not in groups to a CSV file
+try {
+    $UsersNotInGroups | Select-Object UserPrincipalName, DisplayName | Export-Csv -Path $SaveLocation -NoTypeInformation
+}
+
+catch {
+    Write-Error "Error exporting the list of users to a CSV file: $($_.Exception.Message)"
 }
